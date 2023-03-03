f08b222d5e21d1bf8d8a0ca771355070bf431dc3
Enzo Gomes <110132900+enzogomezz@users.noreply.github.com>
2023-03-03 16:26:02 -0300

    Create randomNumber.js

---
+++ randomNumber.js
@@ -0,0 +1,12 @@
+let auto = []
+console.log(auto)
+function blingers() { if (auto.indexOf(1) == -1) {
+    let numeroRandomico = Math.floor(Math.random() * 10) + 1;
+    auto.push(numeroRandomico)
+    console.log(auto)
+    blingers()
+} else {
+    console.log(`fim`)
+}
+}
+blingers(
