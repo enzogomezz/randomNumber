f9152abfb6a0d92a4b58ede67ea86c1e60609980
Enzo Gomes <110132900+enzogomezz@users.noreply.github.com>
2023-06-15 21:02:33 -0300

    just made it a self invoking function

--- randomNumber.js
+++ randomNumber.js
@@ -1,12 +1,11 @@
 let auto = []
-console.log(auto)
-function blingers() { if (auto.indexOf(1) == -1) {
-    let numeroRandomico = Math.floor(Math.random() * 10) + 1;
+;(function blingers() {
+  if (auto.indexOf(1) == -1) {
+    let numeroRandomico = Math.floor(Math.random() * 10) + 1
     auto.push(numeroRandomico)
     console.log(auto)
     blingers()
-} else {
+  } else {
     console.log(`fim`)
-}
-}
-blingers()
+  }
+})()
